9942aa67d5f77df6fa2e5b88fa9182e680e8cf1a
lokihi <90849723+lokihi@users.noreply.github.com>
2022-04-07 02:02:13 +0300

    Add files via upload

--- OpenGLEngine/Vector3.h
+++ OpenGLEngine/Vector3.h
@@ -6,7 +6,7 @@ class Vector3 {
 	public:
 		Vector3(float x, float y, float z);
 		~Vector3() {};
-		const std::vector<float> getCoord();
+		std::vector<float> getCoord();
 		void setCoord(float n_x, float n_y, float n_z);
 
 	private:
@@ -22,7 +22,7 @@ inline Vector3::Vector3(float x, float y, float z)
 	this->z = z;
 }
 
-const std::vector<float> Vector3::getCoord()
+std::vector<float> Vector3::getCoord()
 {
 	std::vector<float> coord;
 	coord.push_back(x);
@@ -38,40 +38,40 @@ inline void Vector3::setCoord(const float n_x, const float n_y, const float n_z)
 	this->z = n_z;
 }
 
-Vector3 operator + (const Vector3 v1, const Vector3 v2)
+Vector3 operator + ( Vector3 v1, Vector3 v2)
 {
-	std::vector<float> coord1;
-	std::vector<float> coord2;
+	std::vector<float> coord1 = v1.getCoord();
+	std::vector<float> coord2 = v2.getCoord();
 
 	return Vector3(coord1[0] + coord2[0], coord1[1] + coord2[1], coord1[2] + coord2[2]);
 }
 
-Vector3 operator - (const Vector3 v1, const Vector3 v2)
+Vector3 operator - (Vector3 v1, Vector3 v2)
 {
-	std::vector<float> coord1;
-	std::vector<float> coord2;
+	std::vector<float> coord1 = v1.getCoord();
+	std::vector<float> coord2 = v2.getCoord();
 
 	return Vector3(coord1[0] - coord2[0], coord1[1] - coord2[1], coord1[2] - coord2[2]);
 }
 
-Vector3 operator / (const Vector3 v, const float a)
+Vector3 operator / (Vector3 v, const float a)
 {
-	std::vector<float> coord;
+	std::vector<float> coord = v.getCoord();
 
 	return Vector3(coord[0] / a, coord[1] / a, coord[2] / a);
 }
 
-Vector3 operator * (const Vector3 v, const float a)
+Vector3 operator * (Vector3 v, const float a)
 {
-	std::vector<float> coord;
+	std::vector<float> coord = v.getCoord();
 
 	return Vector3(coord[0] * a, coord[1] * a, coord[2] * a);
 }
 
-bool operator == (const Vector3 v1, const Vector3 v2)
+bool operator == (Vector3 v1, Vector3 v2)
 {
-	std::vector<float> coord1;
-	std::vector<float> coord2;
+	std::vector<float> coord1 = v1.getCoord();
+	std::vector<float> coord2 = v2.getCoord();
 
 	return ((coord1[0] == coord2[0]) && (coord1[1] == coord2[1]) && (coord1[2] == coord2[2]));
 }
